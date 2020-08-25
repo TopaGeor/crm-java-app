eed825877e65f0e2f8fe30e13ec008f8e9eae50a
Dimitris <iracleous@gmail.com>
2020-08-25 19:39:33 +0300

    Modelling of the crm

--- src/main/java/gr/codehub/commerce/Main.java
+++ src/main/java/gr/codehub/commerce/Main.java
@@ -1,10 +1,16 @@
 package gr.codehub.commerce;
 
+import gr.codehub.commerce.model.Category;
+import gr.codehub.commerce.model.Product;
+
+import gr.codehub.commerce.model.Supplier;
 import gr.codehub.commerce.service.Cart;
 
 public class Main {
     public static void main(String[] args) {
        Cart cart = new Cart();
-
+        Product c = new Product("patata", 1.30,2,
+                Category.SNACKS,
+                new Supplier());
     }
 }
